7fd49d9e43ba3f3e5445de55503fe0b70986fcc7
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-04-23 14:07:25 -0500

    use uncursed prettier config

--- server/dev.js
+++ server/dev.js
@@ -1,54 +1,56 @@
-import { createServer } from '@hattip/adapter-node';
-import { WebSocketServer } from 'ws';
-import { fileURLToPath } from 'node:url';
-import { relative } from 'node:path';
-import chokidar from 'chokidar';
-import { handler } from './index.js';
-import { build } from './build.js';
-import { src } from './utils.js';
+import { createServer } from "@hattip/adapter-node"
+import chokidar from "chokidar"
+import { relative } from "node:path"
+import { fileURLToPath } from "node:url"
+import { WebSocketServer } from "ws"
+import { build } from "./build.js"
+import { handler } from "./index.js"
+import { src } from "./utils.js"
 
-const port = 3000;
+const port = 3000
 
-process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = "development"
 
-createServer(handler).listen(port, 'localhost', async () => {
-	await build();
-	console.log(`⚛️ Future of React started on http://localhost:${port}`);
-});
+createServer(handler).listen(port, "localhost", async () => {
+  await build()
+  console.log(`⚛️ Future of React started on http://localhost:${port}`)
+})
 
 // File watcher to trigger browser refreshes
 // ------------
 
-const refreshPort = 21717;
+const refreshPort = 21717
 
 const wsServer = new WebSocketServer({
-	port: refreshPort
-});
+  port: refreshPort,
+})
 
 /** @type {Set<import('ws').WebSocket>} */
-const sockets = new Set();
+const sockets = new Set()
 
-wsServer.on('connection', (ws) => {
-	sockets.add(ws);
+wsServer.on("connection", (ws) => {
+  sockets.add(ws)
 
-	ws.on('close', () => {
-		sockets.delete(ws);
-	});
+  ws.on("close", () => {
+    sockets.delete(ws)
+  })
 
-	ws.send('connected');
-});
+  ws.send("connected")
+})
 
 /**
  * Watch files in the `app/` directory
  * and trigger a build + refresh on change.
  */
-(async function buildWatch() {
-	chokidar.watch(fileURLToPath(src), { ignoreInitial: true }).on('all', async (event, path) => {
-		console.log('[change]', relative(fileURLToPath(src), path));
-		await build();
+;(async function buildWatch() {
+  chokidar
+    .watch(fileURLToPath(src), { ignoreInitial: true })
+    .on("all", async (event, path) => {
+      console.log("[change]", relative(fileURLToPath(src), path))
+      await build()
 
-		for (const socket of sockets) {
-			socket.send('refresh');
-		}
-	});
-})();
+      for (const socket of sockets) {
+        socket.send("refresh")
+      }
+    })
+})()
--- server/utils.js
+++ server/utils.js
@@ -1,7 +1,7 @@
-import fs from 'node:fs';
+import fs from "node:fs"
 
-export const src = new URL('../app/', import.meta.url);
-export const dist = new URL('../dist/', import.meta.url);
+export const src = new URL("../app/", import.meta.url)
+export const dist = new URL("../dist/", import.meta.url)
 
 /**
  *
@@ -9,7 +9,7 @@ export const dist = new URL('../dist/', import.meta.url);
  * @returns {URL}
  */
 export function resolveSrc(path) {
-	return new URL(path, src);
+  return new URL(path, src)
 }
 
 /**
@@ -18,24 +18,24 @@ export function resolveSrc(path) {
  * @returns {URL}
  */
 export function resolveDist(path) {
-	return new URL(path, dist);
+  return new URL(path, dist)
 }
 
 export function resolveClientDist(path) {
-	return new URL(path, resolveDist('client/'));
+  return new URL(path, resolveDist("client/"))
 }
 
 export function resolveServerDist(path) {
-	return new URL(path, resolveDist('server/'));
+  return new URL(path, resolveDist("server/"))
 }
 
-export const clientComponentMapUrl = resolveDist('clientComponentMap.json');
+export const clientComponentMapUrl = resolveDist("clientComponentMap.json")
 
 export async function writeClientComponentMap(bundleMap) {
-	await fs.promises.writeFile(clientComponentMapUrl, JSON.stringify(bundleMap));
+  await fs.promises.writeFile(clientComponentMapUrl, JSON.stringify(bundleMap))
 }
 
 export async function readClientComponentMap() {
-	const bundleMap = await fs.promises.readFile(clientComponentMapUrl, 'utf-8');
-	return JSON.parse(bundleMap);
+  const bundleMap = await fs.promises.readFile(clientComponentMapUrl, "utf-8")
+  return JSON.parse(bundleMap)
 }
